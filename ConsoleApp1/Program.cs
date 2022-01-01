5c5faccaff30812e57b3a825af576f0f76c5c07e
divya kolaskar <dhanashreekolaskar@gmail.com>
2022-01-01 19:22:25 +0530

    1 Jan

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Loop
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           // char[] Hello = { 'S', 'H', 'R', 'E', 'E' };
+
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("q");
+            arrayList.Add(2);
+            arrayList.Add(2.1);
+            arrayList.Add("q");
+            arrayList.Add("Shree");
+            arrayList.Add("q");
+            arrayList.Add("q");
+           
+
+
+
+
+
+            Console.WriteLine("size:"+arrayList.Capacity);
+            foreach (var h in arrayList)
+            {
+                Console.WriteLine(h);
+            }
+        }
+    }
+}
