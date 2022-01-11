179b6c6ac34a329dcad303e94ec6c1fbf8162492
divya kolaskar <dhanashreekolaskar@gmail.com>
2022-01-11 18:21:11 +0530

    11/01/2022

---
+++ Delegate/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Delegate
+{
+
+    public delegate void Shree(string msg);
+
+    class A
+    {
+        public static void Amazon(string message)
+        {
+            Console.WriteLine("Class A MethodA called:" + message);
+        }
+
+    }
+
+    class B
+    {
+        public static void Flipkart(string message)
+        {
+            Console.WriteLine("Class B Method B:" + message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shree del = A.Amazon;
+            //del("Hiiiiiiii");
+
+            //del = B.Flipkart;
+            //del("Hello");
+
+            //del = (string msg) => Console.WriteLine("Lambda EXP:" + msg);
+            //del("Lambda da da da");
+
+            //......delegate as a parameter 
+
+            static void InvokeDelegate(Shree del)
+            {
+                del("Hello Shree");
+            }
+            Shree del = A.Amazon;
+            InvokeDelegate(del);
+
+            del = B.Flipkart;
+            InvokeDelegate(del);
+
+            del = (string msg) => Console.WriteLine("Lambda is cool:" +msg);
+            InvokeDelegate(del);
+
+            
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Generic
+{
+    //Generic class use <T> IN ANGLE param
+    //it is used after classname
+    public class Store <T>
+    {
+        public T DataStore { get; set; }
+    }
+
+    //multiple Type Param
+
+    class Value <Value1 , Value2>
+    {
+        public Value1 IceCream { get; set; }
+
+        public Value2 Ice { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store<string> store = new Store<string>();
+            store.DataStore ="IceCream";
+            //store.DataStore = 123; cant conv to int to string<string>
+
+            Value<string, string> value = new Value<string, string>();
+            value.IceCream = "Amazing!!!";
+            value.Ice = "Cold";
+        }
+    }
+}
